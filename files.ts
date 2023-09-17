6a89c35108bca006dda51088a09adb97bab111e6
Marek Kobida <marek.kobida@gmail.com>
2023-09-17 16:40:24 +0930

    files.ts

---
+++ files.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2023 Marek Kobida
+ */
+
+const files = [
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/account/accountApplicationRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/account/accountFriendRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/account/accountRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/application/applicationRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/application/applicationSubscribeRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/application/applicationUnsubscribeRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/application/applicationVersionRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/authorization/signInRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/authorization/signOutRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/category/categoryRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/conversation/conversationMessageRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/countryRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/me/meConversationRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/me/meFriendRequestRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/me/meRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/me/meSecretRoute.ts',
+  '/Users/marekkobida/Documents/warden/leopold/server/routes/testRoute.ts',
+];
+
+export default files;
